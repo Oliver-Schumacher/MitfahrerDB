1f6a1ab87eba26a6b25f2eace14b58fa87a9d5d6
Bjoern Buettgenbach <BjoernBuettgenbach@gmx.de>
2022-11-07 13:39:55 +0100

    Zwischenstand Login

---
+++ Backend/MitfahrerDB_Backend/MitfahrerDB_Backend/LoginUser.cs
@@ -0,0 +1,22 @@
+﻿namespace MitfahrerDB_Backend
+{
+    public class LoginUser
+    {
+        public string EmailAdress { get; set;}
+        public string Password { get; set; }
+
+        public string Username { get; set; }
+        public LoginUser(string emailAdress, string password, string username)
+        {
+            EmailAdress = emailAdress;
+            Password = password;
+            Username = username;
+        }
+        public LoginUser()
+        {
+            EmailAdress = "";
+            Password = "";
+            Username = "";
+        }
+    }
+}
